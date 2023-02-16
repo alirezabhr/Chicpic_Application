23f94089c23e81720af2244f960d891ba2b80392
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2023-02-16 12:51:22 +0330

    Create main page with bottom navbar.

---
+++ lib/ui/main/screens/main_screen.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+import 'package:chicpic/ui/main/screens/home_screen.dart';
+import 'package:chicpic/ui/main/screens/explore_screen.dart';
+import 'package:chicpic/ui/main/screens/notifications_screen.dart';
+import 'package:chicpic/ui/main/screens/profile_screen.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _pageIndex = 0;
+
+  final List<Map<String, dynamic>> _mainPages = [
+    {
+      'icon': const Icon(Icons.home_outlined),
+      'page': const HomeScreen(),
+      'label': 'Home',
+    },
+    {
+      'icon': const Icon(Icons.search),
+      'page': const ExploreScreen(),
+      'label': 'Explore',
+    },
+    {
+      'icon': const Icon(Icons.add_box_outlined),
+      'label': 'Upload Image',
+    },
+    {
+      'icon': const Icon(Icons.notifications_none_outlined),
+      'page': const NotificationsScreen(),
+      'label': 'Notifications',
+    },
+    {
+      'icon': const Icon(Icons.person_outline),
+      'page': const ProfileScreen(),
+      'label': 'Profile',
+    },
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: _mainPages[_pageIndex]['page'],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        showUnselectedLabels: false,
+        selectedFontSize: 12,
+        currentIndex: _pageIndex,
+        onTap: (index) {
+          if (_mainPages[index].containsKey('page')) {
+            setState(() {
+              _pageIndex = index;
+            });
+          } else {
+
+          }
+        },
+        items: _mainPages
+            .map((e) => BottomNavigationBarItem(
+                  icon: e['icon'],
+                  label: e['label'],
+                ))
+            .toList(),
+      ),
+    );
+  }
+}
