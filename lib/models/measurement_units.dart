b681d90449e21a87e2ff8f5c483a7bdd8356f1bc
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2023-02-24 14:09:01 +0330

    Create measurement units models, enums and converter methods.

---
+++ lib/models/measurement_units.dart
@@ -0,0 +1,83 @@
+enum LengthUnit {
+  cm('cm', 'cm'),
+  inches('Inches', 'inc.');
+
+  final String name;
+  final String abbreviation;
+
+  const LengthUnit(this.name, this.abbreviation);
+}
+
+class Length {
+  LengthUnit unit;
+  int value;
+
+  Length({
+    this.value = 0,
+    this.unit = LengthUnit.cm,
+  });
+
+  void convertToInches() {
+    if (unit == LengthUnit.cm) {
+      value = (value * 2.54).toInt();
+    }
+    unit = LengthUnit.inches;
+  }
+
+  void convertToCm() {
+    if (unit == LengthUnit.inches) {
+      value = value ~/ 2.54;
+    }
+    unit = LengthUnit.cm;
+  }
+
+  void convert(LengthUnit convertedUnit) {
+    if (convertedUnit == LengthUnit.cm) {
+      convertToCm();
+    } else if (convertedUnit == LengthUnit.inches) {
+      convertToInches();
+    }
+  }
+}
+
+enum MassUnit {
+  kg('Kg', 'Kg'),
+  pound('Pound', 'lbs');
+
+  final String name;
+  final String abbreviation;
+
+  const MassUnit(this.name, this.abbreviation);
+}
+
+class Mass {
+  MassUnit unit;
+  int value;
+
+  Mass({
+    this.value = 0,
+    this.unit = MassUnit.kg,
+  });
+
+  void convertToPound() {
+    if (unit == MassUnit.kg) {
+      value = (value * 2.2).round();
+    }
+    unit = MassUnit.pound;
+  }
+
+  void convertToKg() {
+    if (unit == MassUnit.pound) {
+      value = (value / 2.2).round();
+    }
+    unit = MassUnit.kg;
+  }
+
+  void convert(MassUnit convertedUnit) {
+    if (convertedUnit == MassUnit.kg) {
+      convertToKg();
+    } else if (convertedUnit == MassUnit.pound) {
+      convertToPound();
+    }
+  }
+}
