72c06257e6f9ea8e8a661320d79ce2018b240052
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2023-02-25 17:29:17 +0330

    Add shirt fit and trouser fit images.

--- lib/statics/assets_helper.dart
+++ lib/statics/assets_helper.dart
@@ -1,4 +1,13 @@
 abstract class AssetsHelper {
   static const logo = 'assets/logos/logo.png';
   static const googleIcon = 'assets/icons/google_48_48.png';
+
+  static const _fitsImagesPrefix = 'assets/images/fits/';
+  static const shirtFitSlim = '${_fitsImagesPrefix}shirt_slim.png';
+  static const shirtFitRegular = '${_fitsImagesPrefix}shirt_regular.png';
+  static const trouserFitSkinny = '${_fitsImagesPrefix}trouser_skinny.png';
+  static const trouserFitSlim = '${_fitsImagesPrefix}trouser_slim.png';
+  static const trouserFitNormal = '${_fitsImagesPrefix}trouser_normal.png';
+  static const trouserFitLoose = '${_fitsImagesPrefix}trouser_loose.png';
+  static const trouserFitTapered = '${_fitsImagesPrefix}trouser_tapered.png';
 }
