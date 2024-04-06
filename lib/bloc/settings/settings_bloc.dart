aef5ca68ec3929dcc4dd2e049771d7cb2ab1185c
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2024-04-06 19:58:09 +0330

    Refactor SettingsBloc and SettingsRepository.
     - Add methods in settings repository.
     - make attributes private in settings repo and call methods in settings bloc.

--- lib/bloc/settings/settings_bloc.dart
+++ lib/bloc/settings/settings_bloc.dart
@@ -1,9 +1,7 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:chicpic/repositories/auth/auth_repository.dart';
 import 'package:chicpic/repositories/settings/settings_repository.dart';
-import 'package:chicpic/statics/shared_preferences_keys.dart';
 import 'package:chicpic/models/auth/gender_choices.dart';
 
 part 'settings_event.dart';
@@ -22,18 +20,7 @@ class SettingsBloc extends Bloc<SettingsEvent, SettingsState> {
 
   void _onLoadSettings(LoadSettings event, Emitter<SettingsState> emit) async {
     emit(SettingsLoading());
-
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
-
-    _settingsRepo.showPersonalizedProducts =
-        prefs.getBool(SharedPrefKeys.showPersonalized) ?? false;
-
-    _settingsRepo.lastGenderCategory =
-        prefs.get(SharedPrefKeys.categoryGender) ==
-                GenderChoices.men.abbreviation
-            ? GenderChoices.men
-            : GenderChoices.women;
-
+    await _settingsRepo.loadConfigs();
     emit(SettingsLoaded());
   }
 
@@ -43,27 +30,18 @@ class SettingsBloc extends Bloc<SettingsEvent, SettingsState> {
   ) async {
     emit(SettingsLoading());
 
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
-
     // Check if user additional is null and user wants recommendations
     if (event.showPersonalizedProducts &&
         _authRepo.user!.userAdditional == null) {
       emit(SettingsErrorUserAdditionalRequired());
     } else {
-      prefs.setBool(
-        SharedPrefKeys.showPersonalized,
-        event.showPersonalizedProducts,
-      );
-      _settingsRepo.showPersonalizedProducts = event.showPersonalizedProducts;
+      _settingsRepo.setPersonalizedProducts(event.showPersonalizedProducts);
 
       // Change gender category if user wants recommendations
       if (event.showPersonalizedProducts) {
-        final prefs = await SharedPreferences.getInstance();
         final gender = _authRepo.user!.userAdditional!.genderInterested;
-        prefs.setString(SharedPrefKeys.categoryGender, gender.abbreviation);
-        _settingsRepo.lastGenderCategory = gender;
+        await _settingsRepo.setGenderCategory(gender);
       }
-      //TODO: REFACTOR the above and below code to a function
 
       emit(SettingsShowPersonalizedProductsUpdated());
     }
@@ -74,9 +52,7 @@ class SettingsBloc extends Bloc<SettingsEvent, SettingsState> {
     Emitter<SettingsState> emit,
   ) async {
     emit(SettingsLoading());
-    final prefs = await SharedPreferences.getInstance();
-    prefs.setString(SharedPrefKeys.categoryGender, event.gender.abbreviation);
-    _settingsRepo.lastGenderCategory = event.gender;
+    await _settingsRepo.setGenderCategory(event.gender);
     emit(SettingsLoaded());
   }
 }
--- lib/repositories/settings/settings_repository.dart
+++ lib/repositories/settings/settings_repository.dart
@@ -1,14 +1,40 @@
 import 'package:chicpic/models/auth/gender_choices.dart';
+import 'package:chicpic/statics/shared_preferences_keys.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingsRepository {
-  late bool showPersonalizedProducts;
-  late GenderChoices lastGenderCategory;
+  late bool _showPersonalizedProducts;
+  late GenderChoices _lastGenderCategory;
 
-  // Future<void> saveSettings(Settings settings) async {
-  //   // Save settings to the database
-  // }
-  //
-  // Future<Settings> getSettings() async {
-  //   // Get settings from the database
-  // }
+  SettingsRepository() {
+    loadConfigs();
+  }
+
+  Future<void> loadConfigs() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    _showPersonalizedProducts =
+        prefs.getBool(SharedPrefKeys.showPersonalized) ?? false;
+
+    _lastGenderCategory = prefs.get(SharedPrefKeys.categoryGender) ==
+            GenderChoices.men.abbreviation
+        ? GenderChoices.men
+        : GenderChoices.women;
+  }
+
+  bool get showPersonalizedProducts => _showPersonalizedProducts;
+
+  GenderChoices get lastGenderCategory => _lastGenderCategory;
+
+  Future<void> setPersonalizedProducts(bool value) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setBool(SharedPrefKeys.showPersonalized, value);
+    _showPersonalizedProducts = value;
+  }
+
+  Future<void> setGenderCategory(GenderChoices gender) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString(SharedPrefKeys.categoryGender, gender.abbreviation);
+    _lastGenderCategory = gender;
+  }
 }
